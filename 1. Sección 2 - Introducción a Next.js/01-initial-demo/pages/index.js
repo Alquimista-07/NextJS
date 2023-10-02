5374289a700f34edef27e229a3a0aa012d47377b
Alquimista-07 <jdca.07@gmail.com>
2023-10-02 11:08:27 -0500

    17. Next/Link

--- 1. Sección 2 - Introducción a Next.js/01-initial-demo/pages/index.js
+++ 1. Sección 2 - Introducción a Next.js/01-initial-demo/pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
+import Link from 'next/link'
 
 const inter = Inter({ subsets: ['latin'] })
 // NOTA: Acá tenemos un functional component que no es más que es un simple componente de React, solo que hay ciertas 
@@ -32,7 +33,14 @@ export default function HomePage() {
       <main className={`${styles.main} ${inter.className}`}>
         
         <h1 className={styles.title}>
-          Ir a <a href='/about'>About</a>
+          {/* NOTA: Al algunas veces en donde necesitamos decirle a Next Hey sabes que Next necesito que hasta donde sea posible hagas un prefetch de la siguiente página que puede apuntar a una página en particular
+                    y cuando el usuario vaya a esa página esta puede ser que ya este en memoria, por lo tanto automáticamente no se va a volver a cargar por lo tanto de forma automática se va a realizar la transasición
+                    de forma instantanea y todo esto se hace con el NextLink y se usa llamando la etiqueta Link que se importa. Adicionalmente este prefetch lo podemos desactivar agregando la propieda prefetch={false}
+                    ya que por defecto esta activado, o podemos mandar un objeto con argumentos para mandar otra información, otra opcion que tenemos es el replace, el cual especifica que se evite ir a la página anterior
+                    dando en la flecha del navegador por ejemplo si el usuario ya paso por el login y se autentico ya no sería necesario regresar a él. Podemos revisar todo lo relacionado al Link y sus demás funciones en el sitio web oficial: 
+          */}
+          {/* Ir a <a href='/about'>About</a> */}
+          Ir a <Link href='/about'>About</Link>
         </h1>
 
         <div className={`${styles.center} ${styles.description}`}>
