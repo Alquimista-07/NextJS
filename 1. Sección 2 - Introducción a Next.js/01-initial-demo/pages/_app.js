11e993faf5bf41a97ae170321f0a0b200f09befa
Alquimista-07 <jdca.07@gmail.com>
2023-10-02 09:50:43 -0500

    13. Estructura de directorios en un proyecto

--- 1. Sección 2 - Introducción a Next.js/01-initial-demo/pages/_app.js
+++ 1. Sección 2 - Introducción a Next.js/01-initial-demo/pages/_app.js
@@ -1,5 +1,9 @@
 import '@/styles/globals.css'
 
+// NOTA: Este es el punto central de la aplicación, equivalente al app.js de una aplicación React.
+//       Por lo tanto todas los componentes pasan por acá al momento de constuirsen haciendo que este
+//       archivo sea super importante y no lo debemos borrar. Adicionalmente esto es muy util si queremos
+//       poner un Context (Contexto) que sea disponible a lo largo de la aplicación para todos los componentes
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
